4be693357810cb006401147eb47fd2e2075c5216
BBrewerton <benbrewerton98@gmail.com>
2021-04-07 21:38:08 +1000

    Network variables and base QCD problem variables generated

---
+++ generateIndicatorVector.m
@@ -0,0 +1,7 @@
+function [e] = generateIndicatorVector(index,dimension)
+% This function generates an indicator vector of size dimension with a 1 in
+% the index location and 0's elsewhere
+e = zeros(dimension,1);
+e(index) = 1;
+end
+
